1525146a6acf993d6eef7eab048db40c2fb4f689
beatmartin <bmartin2026@alu.uct.cl>
2026-09-12 17:49:40 -0300

    feat: register habits, categories, and records routers in main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from app.routers import usuarios  
+from routers import usuarios, registros, habitos, categorias 
 app = FastAPI()
 
 app.include_router(usuarios.router)
+app.include_router(registros.router)
+app.include_router(habitos.router)
+app.include_router(categorias.router)  
 
